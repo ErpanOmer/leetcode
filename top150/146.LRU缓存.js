c572f8d103b3ed6bb8da91823c979b6c3c99ec5d
ErpanOmer <354073823@qq.com>
2024-01-29 18:41:50 +0800

    update: 更新算法

--- top150/146.LRU缓存.js
+++ top150/146.LRU缓存.js
@@ -7,18 +7,20 @@ import { LinkToArray } from './common.js'
  * @param {number} capacity
  */
 var LRUCache = function(capacity) {
-    function ListNode(key, val, prev, next) {
-        this.key = (key === undefined ? 0 : key)
-        this.val = (val === undefined ? 0 : val)
-        this.prev = (prev === undefined ? null : prev)
-        this.next = (next === undefined ? null : next)
-    }
-
     this.size = capacity
     this.map = new Map()
-    this.link = new ListNode(-1, -1, null, null)
-    this.last = this.link
+    this.link = new LRUCache.ListNode(-1, -1, null, null)
+    this.last = new LRUCache.ListNode(-2, -2, null, null)
+    this.link.next = this.last
+    this.last.prev = this.link
 };
+
+LRUCache.ListNode = function (key, val, prev, next) {
+    this.key = key
+    this.val = val
+    this.prev = prev
+    this.next = next
+}
 
 /** 
  * @param {number} key
@@ -26,23 +28,14 @@ var LRUCache = function(capacity) {
  */
 LRUCache.prototype.get = function(key) {
     if (this.map.has(key)) {
-        console.log('befor', LinkToArray(this.link.next))
-
         const node = this.map.get(key)
-        const prev = node.prev
-        const next = node.next
+        node.prev.next = node.next
+        node.next && (node.next.prev = node.prev)
 
-        prev.next = next
-        
-        if (next) {
-            next.prev = prev
-        }
-
-        node.prev = this.last
-        this.last = node
-        node.next = null
-
-        console.log(LinkToArray(this.link.next))
+        node.next = this.link.next
+        node.prev = this.link
+        this.link.next.prev = node
+        this.link.next = node        
 
         return node.val
     } else {
@@ -56,49 +49,37 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-    function ListNode(key, val, prev, next) {
-        this.key = (key === undefined ? 0 : key)
-        this.val = (val === undefined ? 0 : val)
-        this.prev = (prev === undefined ? null : prev)
-        this.next = (next === undefined ? null : next)
-    }
-
     if (this.map.has(key)) {
         const node = this.map.get(key)
-        const prev = node.prev
-        const next = node.next
-
-        prev.next = next
-        
-        if (next) {
-            next.prev = prev
-        }
-
-        node.prev = this.last
-        this.last = node
-        node.next = null
-
+        this.get(key)
         node.val = value
     } else {
-        const node = new ListNode(key, value, this.last, null)
-        this.last = this.last.next = node
+        const node = new LRUCache.ListNode(key, value, null, null)
+
+        if (this.map.size >= this.size) {
+            const prev = this.last.prev
+            this.map.delete(prev.key)
+            prev.prev.next = this.last
+            this.last.prev = prev.prev
+            prev.prev = prev.next = null
+        }
+
+        node.next = this.link.next
+        node.prev = this.link
+        this.link.next.prev = node
+        this.link.next = node    
+
         this.map.set(key, node)
     }
 
-
-    if (this.map.size > this.size) {
-        const old = this.link.next
-
-        console.log(old.val, old.next.val)
-
-        this.link.next = old.next
-        old.next = null
-        old.prev = null
-        this.map.delete(old.key)   
-    }
-
-    
+    return null
 };
+
+
+
+// 哈希链表（LinkedHashMap）
+// 时间复杂度：O(1)
+// 空间复杂度：O(1)
 
 const l = new LRUCache(3)
 l.put(1, 1)
@@ -110,4 +91,9 @@ console.log(l.get(3))
 console.log(l.get(2))
 console.log(l.get(1))
 l.put(5, 5)
+console.log(l.get(1))
+console.log(l.get(2))
+console.log(l.get(3))
+console.log(l.get(4))
+console.log(l.get(5))
 
